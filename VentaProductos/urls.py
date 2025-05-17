4253fbb55b9c2f3faaba774d702cd98b83ccc82d
Bukksy <162367828+Bukksy@users.noreply.github.com>
2025-05-17 12:37:44 -0400

    correciones

--- VentaProductos/urls.py
+++ VentaProductos/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('onepiece', views.onepiece, name='onepiece'),
     path('pokemon', views.pokemon, name='pokemon'),
     path('todo', views.todo, name='todo'),
+    path('cartas_pokemon', views.cartas_pokemon, name='cartas_pokemon'),
+    path('cargar-mas-cartas/', views.cargar_mas_cartas, name='cargar_mas_cartas'),
 ]
--- VentaProductos/views.py
+++ VentaProductos/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import JsonResponse
 from django.contrib.auth.decorators import user_passes_test
+import requests
+from django.shortcuts import render
 
 
 def onepiece(request):
@@ -22,6 +24,30 @@ def todo(request):
     productos = Producto.objects.all()
     return render(request, 'Todo.html', {'productos': productos})
 
+def cartas_pokemon(request):
+    return render(request, "cartas.html")  # Solo carga la base y el contenedor
+
+def cargar_mas_cartas(request):
+    page = int(request.GET.get("page", 1))
+    url = f"https://api.pokemontcg.io/v2/cards?page={page}&pageSize=50"
+    headers = {
+        "X-Api-Key": "3ba7564d-df35-4de4-8de5-9a2ab43ff7f3"
+    }
+    response = requests.get(url, headers=headers)
+    cartas = response.json().get("data", [])
+
+    # Extraer solo los datos necesarios para la UI y filtros
+    cartas_data = []
+    for carta in cartas:
+        cartas_data.append({
+            "id": carta.get("id"),
+            "nombre": carta.get("name"),
+            "imagen": carta.get("images", {}).get("small", ""),
+            "rareza": carta.get("rarity", "Desconocida"),
+            "set": carta.get("set", {}).get("name", "Desconocido")
+        })
+
+    return JsonResponse({"cartas": cartas_data})
 
 @login_required
 def agregar_al_carrito(request, producto_id):
